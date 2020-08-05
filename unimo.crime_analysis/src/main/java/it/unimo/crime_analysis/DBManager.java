494a288b1deef49835bde051a50a44ab8414c252
giuseppecastiello <giuseppecastiello@outlook.com>
2020-08-05 19:46:05 +0200

    DBManager changes

--- unimo.crime_analysis/src/main/java/it/unimo/crime_analysis/DBManager.java
+++ unimo.crime_analysis/src/main/java/it/unimo/crime_analysis/DBManager.java
@@ -1,7 +1,7 @@
 package it.unimo.crime_analysis;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -9,24 +9,22 @@ import java.sql.SQLException;
 public class DBManager {
 	public static final String JDBCURL = "jdbc:postgresql://localhost:5532/crime_news?"
 			+ "user=crime_news_ro&password=crime_news_ro";
-			//+ "&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET";
-	protected PreparedStatement statement;
+	protected Statement statement;
 	protected Connection connection;
 
 	public DBManager(String JDBCURL) throws ClassNotFoundException, SQLException {
 		connection = DriverManager.getConnection(JDBCURL);
-		//statement = connection.createStatement();
-		String sql = "SELECT title, description, text FROM crime_news.news LIMIT 2500;";
-		statement = connection.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+		statement = connection.createStatement();
 		statement.setQueryTimeout(30); 
 	}
 
 	public ResultSet executeQuery() throws SQLException {
-		return statement.executeQuery();
+		String sql = "SELECT title, description, text FROM crime_news.news LIMIT 2500;";
+		return statement.executeQuery(sql);
 	}
 
-	public int executeUpdate() throws SQLException {
-		return statement.executeUpdate();
+	public int executeUpdate(String update) throws SQLException {
+		return statement.executeUpdate(update);
 	}
 
 	public void close() throws SQLException {
